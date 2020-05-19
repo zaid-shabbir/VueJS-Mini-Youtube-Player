25ae67f1971fa6a37f70fc0cfb3739e847b6eedf
Zaid Shabbir <zaidshabbir17@gmail.com>
2020-05-19 16:38:33 +0500

    Setup for player

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Player />
+  </div>
+</template>
+
+<script>
+import Player from "./components/Player.vue";
+
+export default {
+  name: "App",
+  components: {
+    Player
+  }
+};
+</script>
